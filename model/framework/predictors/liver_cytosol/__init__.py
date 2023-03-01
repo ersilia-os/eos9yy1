553fa1ba2681bf7119d2bc41a847d326e0c59cd4
pauline banye <paulinebanye@gmail.com>
2023-03-02 00:07:54 +0100

    url error

--- model/framework/predictors/liver_cytosol/__init__.py
+++ model/framework/predictors/liver_cytosol/__init__.py
@@ -1,25 +1,41 @@
 import pickle
+import requests
 import sys
 import os
 import warnings
+warnings.filterwarnings("ignore")
 
 from tqdm import tqdm
+from io import BytesIO
 from os import path
-
-warnings.filterwarnings("ignore")
 
 root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(root, ".."))
 
 
-def download_file(model_number, models_dict):
+def download_file(base_url, model_number, models_dict):
+    hlc_rf_pkl_url = f'{base_url}/model_{model_number}.pkl'
     hlc_model_path = f'../../checkpoints/model_{model_number}.pkl'
-    with open(hlc_model_path, 'rb') as pkl_file:
-        hlc_rf_model = pickle.load(pkl_file)
+    hlc_rf_pkl_file_request = hlc_rf_pkl_url
+    with tqdm.wrapattr(
+        open(os.devnull, "wb"),
+        "write",
+        miniters=1,
+        desc=f'model_{model_number}',
+        total = int(hlc_rf_pkl_file_request.headers['Content-Length'])
+    ) as fout:
+        for chunk in hlc_rf_pkl_file_request.iter_content(chunk_size=4096):
+            fout.write(chunk)
+    with open(hlc_model_path, 'wb') as hlc_rf_pkl_file_writer:
+        hlc_rf_pkl_file_writer.write(hlc_rf_pkl_file_request.content)
+
+    hlc_rf_model = pickle.load(BytesIO(hlc_rf_pkl_file_request.content))
     return hlc_rf_model
 
-
 def load_models():
+    base_url = os.path.abspath(os.path.join(root, '../../checkpoints'))
+    # base_url = 'file://' + os.path.abspath(os.path.join(root, '../../checkpoints'))
+    # base_url = 'https://opendata.ncats.nih.gov/public/adme/models/current/static/liver_cytosol/'
     print(f'Loading human liver cytosol stability random forest models', file=sys.stdout)
 
     hlc_models_dict = {}
@@ -31,7 +47,7 @@ def load_models():
                 hlc_models_dict[f'model_{model_number}'] = pickle.load(pkl_file)
         else:
             os.makedirs(f'../../checkpoints', exist_ok=True)
-            hlc_models_dict[f'model_{model_number}'] = download_file(model_number, hlc_models_dict)
+            hlc_models_dict[f'model_{model_number}'] = download_file(base_url, model_number, hlc_models_dict)
 
     print(f'Finished loading human liver cytosol stability models', file=sys.stdout)
     return hlc_models_dict
