ba0e16eae9352f6f57ce006c8d2a94de98ff5f0b
Pauline.B <paulinebanye@gmail.com>
2023-03-02 10:56:48 +0100

    new clone after revert

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
 FROM bentoml/model-server:0.11.0-py37
 MAINTAINER ersilia
 
+RUN pip install torch==1.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 RUN pip install rdkit
+RUN pip install numpy
+RUN pip install pandas
+RUN pip install tqdm
+RUN pip install typing-extensions
+RUN pip install typed-argument-parser
+RUN pip install tensorboardX
+RUN pip install scikit-learn
+RUN pip install hyperopt
+RUN pip install requests
 
 WORKDIR /repo
 COPY . /repo
---
+++ model/framework/predictors/features/descriptor_gen.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
+
+class DescriptorGen:
+
+    def from_smiles(self,smi):
+        mol = Chem.MolFromSmiles(smi)
+        if mol:
+            return self.from_mol(mol)
+        else:
+            return None
+
+    def from_mol(self,mol):
+        fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2)
+        arr = np.zeros((0,), dtype=np.int8)
+        DataStructs.ConvertToNumpyArray(fp,arr)
+        return arr
+
+if __name__ == "__main__":
+    descriptor_gen = DescriptorGen()
+    print(descriptor_gen.from_smiles("CCCC"))
